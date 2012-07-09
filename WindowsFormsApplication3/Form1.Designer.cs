82a0d5b367e0395a8e270182cd74d7d1e647452a
JohnnyPP <johnnnyppp@gmail.com>
2012-07-10 00:37:01 +0200

    SHT15_120710

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Temperature = new System.Windows.Forms.Label();
+            this.Humidity = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(26, 31);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(167, 31);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Temperature
+            // 
+            this.Temperature.AutoSize = true;
+            this.Temperature.Location = new System.Drawing.Point(26, 159);
+            this.Temperature.Name = "Temperature";
+            this.Temperature.Size = new System.Drawing.Size(67, 13);
+            this.Temperature.TabIndex = 3;
+            this.Temperature.Text = "Temperature";
+            // 
+            // Humidity
+            // 
+            this.Humidity.AutoSize = true;
+            this.Humidity.Location = new System.Drawing.Point(26, 206);
+            this.Humidity.Name = "Humidity";
+            this.Humidity.Size = new System.Drawing.Size(47, 13);
+            this.Humidity.TabIndex = 4;
+            this.Humidity.Text = "Humidity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Title = "Time [s]";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            chartArea1.AxisY.Title = "Temperature [°C]";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(316, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(542, 293);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            title1.Name = "Sensirion";
+            title1.Text = "Sensirion SHT15 Temperatrure sensor";
+            this.chart1.Titles.Add(title1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // chart2
+            // 
+            chartArea2.AxisX.Title = "Time [s]";
+            chartArea2.AxisX.TitleFont = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            chartArea2.AxisY.Title = "Relative humidity [%]";
+            chartArea2.AxisY.TitleFont = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(316, 324);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(542, 293);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            title2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            title2.Name = "Title1";
+            title2.Text = "Sensirion SHT Humidity sensor";
+            this.chart2.Titles.Add(title2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 623);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Humidity);
+            this.Controls.Add(this.Temperature);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "SHT15 Sensor data";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Temperature;
+        private System.Windows.Forms.Label Humidity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;      //for plotting
+using MathNet.Numerics.Statistics;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        private int i = 0;
+        private DateTime dtStarttime;
+        private TimeSpan tsTimespent;
+
+        private List<double> listdTimespent = new List<double>();
+        private List<double> listdTemperature = new List<double>();
+        private List<double> listdHumidity = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            dtStarttime = DateTime.Now;
+
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.DtrEnable = true;
+            serialPort1.Open();
+
+            serialPort1.DataReceived += serialPort1_DataReceived;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string line = serialPort1.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+        }
+
+        private delegate void LineReceivedEvent(string line);
+
+        private void LineReceived(string line)
+        {
+            //System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            //sw.Start();
+
+            double dTemperature, dTemperatureRound, dHumidity;
+            //What to do with the received line here
+            label1.Text = line;
+
+            try
+            {
+                DateTime dtStart = DateTime.Now;
+                Temperature.Text = line.Substring(13, 13);
+                Humidity.Text = line.Substring(38, 5);
+
+                dTemperature = double.Parse(line.Substring(13, 13), System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo);  //Arduino sends text as 123.335 we need to change it to 123,335 to succesfully Parse it to double
+                dHumidity = double.Parse(line.Substring(38, 5), System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.NumberFormatInfo.InvariantInfo);
+
+                dTemperatureRound = Math.Round(dTemperature, 3);
+
+                //////////////////////////////TEMPERATURE
+
+                chart1.Series[0].BorderWidth = 2;
+                chart1.Series[0].ChartType = SeriesChartType.Line;
+
+                //chart1.Titles.Clear();
+                //chart1.Titles.Add("TMP102 Temperatur Sensor");
+                //chart1.Titles[0].Font = new Font("Arial", 12f);
+                //chart1.ChartAreas[0].AxisX.Title = "Zeit [s]";
+                //chart1.ChartAreas[0].AxisY.Title = "Temperatur [°C]";
+                //chart1.ChartAreas[0].AxisX.TitleFont = new Font("Arial", 12f);
+                //chart1.ChartAreas[0].AxisY.TitleFont = new Font("Arial", 12f);
+
+                //chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Arial", 18);
+                //this.chart1.PrimaryXAxis.Font = new Font("Verdana", 8f, FontStyle.Bold);
+
+                tsTimespent = DateTime.Now - dtStarttime;
+                listdTimespent.Add(i);
+                listdTemperature.Add(dTemperatureRound);
+
+                chart1.Series[0].Points.AddXY(tsTimespent.TotalSeconds, dTemperatureRound);
+                chart1.ChartAreas["ChartArea1"].AxisX.Minimum = listdTimespent[0];
+                chart1.ChartAreas["ChartArea1"].AxisY.Minimum = (listdTemperature.Min() - 0.5);
+                chart1.ChartAreas["ChartArea1"].AxisY.Maximum = (listdTemperature.Max() + 0.5);
+                //chart1.ChartAreas["ChartArea1"].AxisX = new Axis { LabelStyle = new LabelStyle() { Font = new Font("Verdana", 7.5f) } };
+                //chart1.ChartAreas["ChartArea1"].AxisX.TitleFont = "Verdana";
+
+                //chart1.ChartAreas["ChartArea1"].AxisX.Title = "Zeit [s]";
+                //chart1.ChartAreas["ChartArea1"].AxisY.Title = "Temparature [C]";
+
+                DateTime time = DateTime.Now;
+                //string format = "MMM ddd d HH:mm yyyy";
+                string format = "yyyyMMddHHmmssffff";
+
+                label2.Text = time.ToString(format);
+
+                i++;
+
+                // DateTime dtStop = DateTime.Now;
+                // DateTime dtDifference;
+
+                // dtStop.Subtract(dtStop);
+
+                /////////////////////////////////HUMIDTY
+
+                chart2.Series[0].BorderWidth = 2;
+                chart2.Series[0].ChartType = SeriesChartType.Line;
+                chart2.Series[0].Points.AddXY(tsTimespent.TotalSeconds, dHumidity);
+                listdHumidity.Add(dHumidity);
+                chart2.ChartAreas["ChartArea1"].AxisX.Minimum = listdTimespent[0];
+                chart2.ChartAreas["ChartArea1"].AxisY.Minimum = (listdHumidity.Min() - 0.5);
+                chart2.ChartAreas["ChartArea1"].AxisY.Maximum = (listdHumidity.Max() + 0.5);
+                listdHumidity.StandardDeviation();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            //sw.Stop();
+
+            //TimeSpan timespent = DateTime.Now - starttime;
+
+            //label3.Text = tsTimespent.TotalSeconds.ToString();
+            label3.Text = Convert.ToString(listdTemperature.Average());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+    }
+}
